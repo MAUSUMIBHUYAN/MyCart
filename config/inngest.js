c73c78933729a40ab1d330c76dd8c27a97f5df47
Mausumi Bhuyan <122201001@smail.iitpkd.ac.in>
2025-07-12 10:55:50 +0530

    first commit

---
+++ config/inngest.js
@@ -0,0 +1,70 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "MyCart-next" });
+
+/**
+ * Inngest Function to save user data to a database on user creation
+ */
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: 'sync-user-from-clerk'
+  },
+  { event: 'clerk/user.created' },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      imageUrl: image_url
+    };
+    await connectDB();
+    await User.create(userData);
+  }
+);
+
+/**
+ * Inngest Function to update user data in database on user update
+ */
+export const syncUserUpdation = inngest.createFunction(
+  {
+    id: 'update-user-from-clerk'
+  },
+  { event: 'clerk/user.updated' },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      imageUrl: image_url
+    };
+    await connectDB();
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+/**
+ * Inngest Function to delete user from database on user deletion
+ */
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: 'delete-user-with-clerk'
+  },
+  { event: 'clerk/user.deleted' },
+  async ({ event }) => {
+    const { id } = event.data;
+    await connectDB();
+    await User.findByIdAndDelete(id);
+  }
+);
+
+// ✅ Export all functions
+export {
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion
+};
